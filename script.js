cbcc8fa73941e18be5f3279309d1347551f611a6
StefanMogren <stefan_mogren@hotmail.com>
2024-12-19 14:53:28 +0100

    Added so input size increases with letters typed

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 const numberInputHTML = document.getElementById("number-input")
 const convertBtn = document.getElementById("convert-btn")
-numberInputHTML.value = "gafa"
+const myContentP = document.getElementById("check-content-p")
 
+convertBtn.addEventListener
+
+numberInputHTML.addEventListener('input', this.typing)
+
+function typing(e) {
+    e.target.setAttribute('size', e.target.value.length);
+}
+
+
+/*
+convertBtn.addEventListener('click', function() {
+    let test = myContentP.getContext("2d");
+    let text = test.measureText("Hello")
+    console.log(text)
+})
+*/
