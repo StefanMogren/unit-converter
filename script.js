ab3c5b78712e7427757c20c051f31ad388536ad3
StefanMogren <stefan_mogren@hotmail.com>
2025-01-01 10:23:46 +0100

    Created centimeterInchConverter

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-const numberInputHTML = document.getElementById("number-input")
 const convertBtn = document.getElementById("convert-btn")
-const myContentP = document.getElementById("check-content-p")
+// convertBtn.addEventListener
 
-convertBtn.addEventListener
 
+const numberInputHTML = document.getElementById("number-input")
 numberInputHTML.addEventListener('input', this.typing)
-
 function typing(e) {
     e.target.setAttribute('size', e.target.value.length);
     if(!e.target.value) {
@@ -14,11 +12,30 @@ function typing(e) {
     
 }
 
+const meterFoot = document.getElementById("meter-foot")
+const kilometerMile = document.getElementById("kilometer-mile")
+const literGallon = document.getElementById("liter-gallon")
+const gramOunce = document.getElementById("gram-ounce")
+const kilogramPound = document.getElementById("kilogram-pound")
+
+
+const centimeterInchHTML = document.getElementById("centimeter-inch");
+function centimeterInchConverter (value) {
+    let formula = 2.54;
+    let centToInch = Math.round((value/formula)*1000)/1000;
+    let inchToCent = value*formula
+    
+    centimeterInchHTML.textContent = `${value} centimeters = ${centToInch} inches`
+    centimeterInchHTML.innerHTML += "<br>"
+    
+    centimeterInchHTML.innerText += `${value} inches = ${inchToCent} centimeters`;
+}
+centimeterInchConverter(20)
 
 /*
 convertBtn.addEventListener('click', function() {
-    let test = myContentP.getContext("2d");
-    let text = test.measureText("Hello")
-    console.log(text)
+let test = myContentP.getContext("2d");
+let text = test.measureText("Hello")
+console.log(text)
 })
 */
