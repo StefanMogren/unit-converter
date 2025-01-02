74d4085632c1d332d3efb95b6872395651010039
StefanMogren <stefan_mogren@hotmail.com>
2025-01-02 08:17:09 +0100

    Added meterFootConverter

--- script.js
+++ script.js
@@ -12,25 +12,36 @@ function typing(e) {
     
 }
 
-const meterFoot = document.getElementById("meter-foot")
-const kilometerMile = document.getElementById("kilometer-mile")
-const literGallon = document.getElementById("liter-gallon")
-const gramOunce = document.getElementById("gram-ounce")
-const kilogramPound = document.getElementById("kilogram-pound")
+
+const kilometerMileHTML = document.getElementById("kilometer-mile")
+const literGallonHTML = document.getElementById("liter-gallon")
+const gramOunceHTML = document.getElementById("gram-ounce")
+const kilogramPoundHTML = document.getElementById("kilogram-pound")
 
 
 const centimeterInchHTML = document.getElementById("centimeter-inch");
 function centimeterInchConverter (value) {
     let formula = 2.54;
     let centToInch = Math.round((value/formula)*1000)/1000;
-    let inchToCent = value*formula
+    let inchToCent = value*formula;
     
-    centimeterInchHTML.textContent = `${value} centimeters = ${centToInch} inches`
-    centimeterInchHTML.innerHTML += "<br>"
-    
+    centimeterInchHTML.textContent = `${value} centimeters = ${centToInch} inches`;
+    centimeterInchHTML.innerHTML += "<br>";
     centimeterInchHTML.innerText += `${value} inches = ${inchToCent} centimeters`;
 }
-centimeterInchConverter(20)
+centimeterInchConverter(20);
+
+
+const meterFootHTML = document.getElementById("meter-foot")
+function meterFootConverter (value) {
+    let formula = 3.281;
+    let meterToFoot = value * formula;
+    let footToMeter = Math.round((value/formula)*1000)/1000;
+    meterFootHTML.textContent = `${value} meters = ${meterToFoot} feet`;
+    meterFootHTML.innerHTML += "<br>";
+    meterFootHTML.innerText += `${value} feet = ${footToMeter} meters`;
+}
+meterFootConverter(20);
 
 /*
 convertBtn.addEventListener('click', function() {
